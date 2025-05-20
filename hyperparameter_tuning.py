48fdc72d8a478ec73a99d9f9e716ea6189e24912
dhalvor <d.halverson@outlook.com>
2025-05-20 12:47:48 +1000

    Added fortran library file

--- hyperparameter_tuning.py
+++ hyperparameter_tuning.py
@@ -14,11 +14,12 @@ init_temp = 10
 w1_max = 2*np.pi*40e6
 
 # Define parameters to be changed
-cooling_rate_array = np.array([0.5, 0.6, 0.7, 0.8, 0.9, 0.95, 0.99])
-results_array = np.zeros([3, len(cooling_rate_array)])
+# cooling_rate_array = np.array([0.5, 0.6, 0.7, 0.8, 0.9, 0.95, 0.99])
+cooling_rate_array = np.array([0.5, 0.6, 0.7])
+results_array = np.zeros([5, len(cooling_rate_array)])
 
 for c in range(len(cooling_rate_array)):
-    for i in range(3):
+    for i in range(5):
         cooling_rate = cooling_rate_array[c]
         opt = SimulatedAnnealing()
         coeffs, error =  opt.run_annealing(
@@ -33,5 +34,12 @@ for c in range(len(cooling_rate_array)):
             cooling_rate=cooling_rate,
             w1_max=w1_max
         )
+        results_array[i, c] = error
 
-        
+plt.plot(cooling_rate_array, results_array[0, :], '.--')
+plt.plot(cooling_rate_array, results_array[1, :], '.--')
+plt.plot(cooling_rate_array, results_array[2, :], '.--')
+plt.plot(cooling_rate_array, results_array[3, :], '.--')
+plt.plot(cooling_rate_array, results_array[4, :], '.--')
+plt.show()
+
